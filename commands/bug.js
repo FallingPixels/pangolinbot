a36a14f7027fcb9c19cf3b5be5cbb4a68d1d4f8f
Humberto Navarro <humberto.navarro.dev@gmail.com>
2021-07-26 01:05:56 -0700

    bug report command

---
+++ commands/bug.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'bug',
+	description: 'Returns info on how to submit a bug report.',
+	execute(message, args) {
+    message.reply('To submit a bug report, put in an issue at: \n https://github.com/FallingPixels/pangolinbot/issues');
+	},
+};
